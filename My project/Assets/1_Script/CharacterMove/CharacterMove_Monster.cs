ffbba19736e4834053bf6e92d0d5709e7b41dd5a
Xamor <103027877+ChocoBerry13@users.noreply.github.com>
2026-07-20 15:56:51 +0900

    일정거리따라오기/사이드뷰전환

--- My project/Assets/1_Script/CharacterMove/CharacterMove_Monster.cs
+++ My project/Assets/1_Script/CharacterMove/CharacterMove_Monster.cs
@@ -8,7 +8,18 @@ public class CharacterMove_Monster : MonoBehaviour
    [SerializeField] Transform target;
     [SerializeField] float moveSpeed = 2.0f;
     [SerializeField] float fllowDistance = 1.0f;
+
+
+
+    [SerializeField] SpriteRenderer spriteRenderer;
+
+    [SerializeField] bool isSide=false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+        spriteRenderer = this.GetComponent<SpriteRenderer>(); 
+    }
+
     void Start()
     {
 
@@ -17,6 +28,23 @@ public class CharacterMove_Monster : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isSide)
+        {
+            MoveSideView();
+        }
+
+        else
+        {
+            Move4direction();
+        }
+
+        Debug.Log(isSide);
+
+    }
+
+    void Move4direction()
+    {
+
         Vector3 direction = target.position - this.transform.position;
         Vector3 lookDirection = direction.normalized; // lookDirection에 정규화된 방향 벡터를 넣었습니다.
         float distance = direction.magnitude; // float distance = Vector3.Distance(transform.position, target.position);
@@ -24,11 +52,22 @@ public class CharacterMove_Monster : MonoBehaviour
         if (lookDirection != Vector3.zero)
         // Vector3.zero는 (0,0,0)과 같습니다.
         {
-           // transform.up = lookDirection; // 객체의 위쪽 방향을 바라보는 방향으로 설정한다.
+            // transform.up = lookDirection; // 객체의 위쪽 방향을 바라보는 방향으로 설정한다.
             transform.right = lookDirection;
         }
-        if (distance > fllowDistance)
+        if (distance > fllowDistance) // 혹은 Vector3.Distance(transform.position, targetTransform.position) > 1 (거리가 1보다 커야 움직이겟다)
             transform.position = Vector3.MoveTowards(this.transform.position, target.position, moveSpeed * Time.deltaTime);
+
+    }
+    void MoveSideView()
+    {
+        Vector3 direction = target.position - this.transform.position;
+        Vector3 lookDirection = new Vector3(direction.x,0,0).normalized; // lookDirection에 정규화된 방향 벡터를 넣었습니다.
+
+        spriteRenderer.flipX = lookDirection.x < 0; //이걸로 반전여부가 정해짐 
+
+        // lookDirection 방향으로 moveSpeed 속도로 이동한다. (좌우로만. 사이드뷰 한계 !) 
+        transform.position += lookDirection * moveSpeed * Time.deltaTime;
 
     }
 
