a013b7435f0b37cdeb7fe609127691f07f90416e
Xamor <103027877+ChocoBerry13@users.noreply.github.com>
2026-07-20 18:26:54 +0900

    키보드 무빙 완결
    flip.x를 다루기위한 SpriteRenderer GetComponenet 라던지 InputSystem 세팅들 (send message 기반이면 update호출하지않아도됨) 등

--- My project/Assets/1_Script/PlayerControl.cs
+++ My project/Assets/1_Script/PlayerControl.cs
@@ -1,9 +1,18 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEngine.GraphicsBuffer;
 
 public class PlayerControl : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 5f;
+    Vector2 moveInput;//이동방향을 담음
+    SpriteRenderer spriteRenderer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+        //inspector창 말고 코드안에서 this. 겟컴포넌트해옴
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+    }
     void Start()
     {
         
@@ -12,12 +21,24 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (moveInput != Vector2.zero)
+        {
+            //v2입력을v3로 포지셔에 전달
+            //
+            Vector3 moveDirection = new Vector3(moveInput.x, moveInput.y, 0f);
+            transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+
+
+            // lookDirection.X가 0보다 작다 = 타겟이 왼쪽에 있다 → flipX를 true로 설정한다.
+            spriteRenderer.flipX = moveDirection.x < 0;
+        }
     }
 
     void OnMove(InputValue _value)
     {
-        //
-        Vector2 movePosition = _value.Get<Vector2>();
-        UnityEngine.Debug.Log(movePosition);
+        //// 매개변수로 받아온 _value 의 Vector2를 moveInput에 넣음
+        moveInput = _value.Get<Vector2>();
+        UnityEngine.Debug.Log(moveInput);
     }
 }
