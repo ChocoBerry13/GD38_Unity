0b27b40b4b8665e53117b755b3178dae1a65e446
Xamor <103027877+ChocoBerry13@users.noreply.github.com>
2026-07-20 14:21:10 +0900

    쫒아오는 적

---
+++ My project/Assets/1_Script/CharacterMove/CharacterMove_Monster.cs
@@ -0,0 +1,34 @@
+using System;
+using TMPro;
+using Unity.Hierarchy;
+using UnityEngine;
+
+public class CharacterMove_Monster : MonoBehaviour
+{
+   [SerializeField] Transform target;
+
+
+   [SerializeField] float moveSpeed = 2.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        Vector3 lookDirection = direction.normalized; // lookDirection에 정규화된 방향 벡터를 넣었습니다.
+        float distance = direction.magnitude; // float distance = Vector3.Distance(transform.position, target.position);
+        // 자신과 타겟의 거리가 0이 아닌 경우에만 동작합니다.
+        if (lookDirection != Vector3.zero) // Vector3.zero는 (0,0,0)과 같습니다.
+        {
+            transform.up = lookDirection; // 객체의 위쪽 방향을 바라보는 방향으로 설정한다.
+        }
+       transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+
+    }
+
+
+}
